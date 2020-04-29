3f99d9e6e68ce36e53bcfef125513a4314ce72a9
Michal Koperski <michal.koperski@tooploox.com>
2020-04-29 15:25:51 +0200

    Download environment script

---
+++ download_environment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+WORKSPACE_DIR="workspace/sun397"
+
+
+fileId=1S_7eulwWjdzHYbyjezLMwZSjH8EbjF9O
+fileName=plugin_networks_workspace.tar.gz
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
+code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
+
+tar xvf ${fileName}
+rm ${fileName}
+
+#TODO Download SUN397
+#if [[ -f SUN397.tar.gz ]]; then
+#    wget http://vision.princeton.edu/projects/2010/SUN/SUN397.tar.gz
+#fi
+#
+#if [ ! -d ${WORKSPACE}/images ]; then
+#    #unpack
+#fi
